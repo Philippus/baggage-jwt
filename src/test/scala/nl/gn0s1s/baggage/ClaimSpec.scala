82d132758afb4f6746b86deb84cec797f8b4c3e3
Philippus Baalman <philippus@gmail.com>
2017-12-17 15:15:07 +0100

    Move registered claim generator to generators

--- src/test/scala/nl/gn0s1s/baggage/ClaimSpec.scala
+++ src/test/scala/nl/gn0s1s/baggage/ClaimSpec.scala
@@ -1,19 +1,12 @@
 package nl.gn0s1s.baggage
 
 import org.scalacheck._
-import org.scalacheck.Gen.oneOf
 import org.scalacheck.Prop.{ forAll, BooleanOperators }
 
 import claim._
 
+import Generators._
 object ClaimSpec extends Properties("Claim") {
-  def genRegisteredClaim: Gen[Claim] = for {
-    claimName <- oneOf(List("iss", "sub", "aud", "jti", "exp", "nbf", "iat"))
-    claimValue <- if (List("iss", "sub", "aud", "jti").contains(claimName)) Gen.alphaNumStr else Gen.posNum[Int]
-  } yield Claim(claimName, claimValue)
-
-  implicit val arbitraryClaim: Arbitrary[Claim] = Arbitrary(genRegisteredClaim)
-
   property("issuer claim accepts stringOrUri") = forAll {
     s: String =>
       !s.contains(':') ==> IssuerClaim(s).isValid
