ae455042f5488c5aca0a52abddf7280ef5de3ff7
Philippus Baalman <philippus@gmail.com>
2017-12-20 22:35:53 +0100

    Extract checks into separate methods

--- src/main/scala/nl/gn0s1s/baggage/claim/ClaimsProcessor.scala
+++ src/main/scala/nl/gn0s1s/baggage/claim/ClaimsProcessor.scala
@@ -7,42 +7,72 @@ import scala.util._
 import ClaimsSet.ClaimsSet
 
 object ClaimsProcessor {
-  def process(claims: ClaimsSet, reqClaimNames: Set[String], expectedClaims: ClaimsSet, clockSkew: Duration): Try[ClaimsSet] = {
-    // required claim names should be in the supplied claims
-    val containsAllReqClaims = reqClaimNames -- claims.map(_.name) == Set.empty
+  def checkRequiredClaims(claims: ClaimsSet, reqClaimNames: Set[String]): Try[ClaimsSet] = {
+    if (reqClaimNames -- claims.map(_.name) == Set.empty)
+      Success(claims)
+    else
+      Failure(new IllegalArgumentException("Not all required claim names present in claims set"))
+  }
 
-    // expected claims should be in the supplied claims (audience claim is handled separately)
-    val expectedClaimsArePresent = expectedClaims.filterNot(_.name == "aud") -- claims == Set.empty
+  // expected claims should be in the supplied claims
+  def checkExpectedClaims(claims: ClaimsSet, expectedClaims: ClaimsSet): Try[ClaimsSet] = {
+    if (expectedClaims -- claims == Set.empty)
+      Success(claims)
+    else
+      Failure(new IllegalArgumentException("Not all expected claims match claims in set"))
+  }
 
-    // check if expected audience claim value is present in supplied set of claims
-    val checkAudienceClaim = if (expectedClaims.exists(_.name == "aud")) {
-      val audExpClaim = expectedClaims.filter(_.name == "aud").head
-      claims.filter(_.name == "aud").forall(_.value match {
-        case l: List[Any] => l.contains(audExpClaim.value)
-        case x => x == audExpClaim.value
-      })
-    } else true
+  // check if expected audience claim value is present in supplied set of claims
+  def checkAudienceClaim(claims: ClaimsSet, audienceClaim: Option[Claim]): Try[ClaimsSet] = {
+    val checkAudienceClaim = claims.filter(_.name == "aud").forall(_.value match {
+      case l: List[Any] => l.exists(x => audienceClaim.exists(_.value == x))
+      case x => audienceClaim.exists(_.value == x)
+    })
 
+    if (checkAudienceClaim)
+      Success(claims)
+    else
+      Failure(new IllegalArgumentException("Audience claim does not match audience claim in set"))
+  }
+
+  def checkExpirationTimeClaim(claims: ClaimsSet, clockSkew: Duration): Try[ClaimsSet] = {
     val current = LocalDateTime.now()
 
-    // if expiration time claim is present it should be checked
-    val checkExpirationTimeClaim = if (claims.exists(_.name == "exp")) {
-      val expirationTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(claims.filter(_.name == "exp").head.value match { case l: Long => l }), ZoneOffset.UTC)
-      current.isBefore(expirationTime.plus(clockSkew))
-    } else true
+    val checkExpirationTimeClaim = claims.filter(_.name == "exp").forall(_.value match {
+      case l: Long =>
+        val expirationTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(l), ZoneOffset.UTC)
+        current.isBefore(expirationTime.plus(clockSkew))
+    })
 
-    // if not before claim is present it should be checked
-    val checkNotBeforeClaim = if (claims.exists(_.name == "nbf")) {
-      val notBeforeTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(claims.filter(_.name == "nbf").head.value match { case l: Long => l }), ZoneOffset.UTC)
-      !current.isBefore(notBeforeTime.minus(clockSkew))
-    } else true
+    if (checkExpirationTimeClaim)
+      Success(claims)
+    else
+      Failure(new IllegalArgumentException("Token has expired"))
+  }
 
+  // if not before claim is present it should be checked
+  def checkNotBeforeClaim(claims: ClaimsSet, clockSkew: Duration): Try[ClaimsSet] = {
+    val current = LocalDateTime.now()
+
+    val checkNotBeforeClaim = claims.filter(_.name == "nbf").forall(_.value match {
+      case l: Long =>
+        val notBeforeTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(l), ZoneOffset.UTC)
+        !current.isBefore(notBeforeTime.minus(clockSkew))
+    })
+
+    if (checkNotBeforeClaim)
+      Success(claims)
+    else
+      Failure(new IllegalArgumentException("Token not valid yet"))
+  }
+
+  def process(claims: ClaimsSet, reqClaimNames: Set[String], expectedClaims: ClaimsSet, clockSkew: Duration): Try[ClaimsSet] = {
     for {
-      claims <- if (containsAllReqClaims) Success(claims) else Failure(new IllegalArgumentException("Not all required claim names present in claims set"))
-      claims <- if (expectedClaimsArePresent) Success(claims) else Failure(new IllegalArgumentException("Not all expected claims match claims in set"))
-      claims <- if (checkAudienceClaim) Success(claims) else Failure(new IllegalArgumentException("Audience claim does not match audience claim in set"))
-      claims <- if (checkExpirationTimeClaim) Success(claims) else Failure(new IllegalArgumentException("Token has expired"))
-      claims <- if (checkNotBeforeClaim) Success(claims) else Failure(new IllegalArgumentException("Token not valid yet"))
+      claims <- checkRequiredClaims(claims, reqClaimNames)
+      claims <- checkExpectedClaims(claims, expectedClaims.filterNot(_.name == "aud"))
+      claims <- checkAudienceClaim(claims, expectedClaims.find(_.name == "aud"))
+      claims <- checkExpirationTimeClaim(claims, clockSkew)
+      claims <- checkNotBeforeClaim(claims, clockSkew)
     } yield claims
   }
 }
